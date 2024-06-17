e3c9af4cf4bd7b38f246eab0be5b89ad3f3bfa3a
001osama <osamanasim909@gmail.com>
2024-06-17 20:36:28 +0500

    prejoin underdevelopment

---
+++ src/app/(tabs)/join-room/prejoin.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import { useEffect } from "react";
+import { useCameraPermissions, useMicrophonePermissions } from "expo-camera";
+
+// We use `requestMicrophonePermissionsAsync` method to request microhone usage permission
+
+const Prejoin = () => {
+ 
+
+  return (
+    <View>
+      <Text>Prejoin working</Text>
+    </View>
+  );
+};
+
+export default Prejoin;
